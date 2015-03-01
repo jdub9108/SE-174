d883ab9ee063f69a042553ef18104edc2f6e1bf4
Dhruv Mevada <mevadadhruv90@gmail.com>
2015-03-01 11:08:27 -0800

    Added proper prepared statement support for registration

--- php/register.php
+++ php/register.php
@@ -20,16 +20,17 @@ echo "Came here <br>";
 		$con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
     	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    	$query = "insert into users values(null, ?, ?, ?, ?, ?, ?);";
-    	$prepared_statement = $con->prepare($query);
-    	$prepared_statement->execute(array(
-    		 $first_name,
-    		 $user_name,
-    		 $last_name,
-    		 $password,
-    		 0,
-    		 0));
-    	
+    	$query = "insert into users values(null, :first_name, :user_name, :last_name, :password, :books_sold, :books_bought);";
+        $prepared_statement = $con->prepare($query);
+        
+        $prepared_statement->bindValue(':first_name', $first_name, PDO::PARAM_STR);
+        $prepared_statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
+        $prepared_statement->bindValue(':last_name', $last_name, PDO::PARAM_STR);
+        $prepared_statement->bindValue(':password', $password, PDO::PARAM_STR);
+        $prepared_statement->bindValue(':books_sold', 0, PDO::PARAM_INT);
+        $prepared_statement->bindValue(':books_bought', 0, PDO::PARAM_INT);
+
+        $prepared_statement->execute();
     	$con = null;
 	}
 ?>
