794e10d09b342be1f139706b8dbcd2aabc774816
Dhruv Mevada <mevadadhruv90@gmail.com>
2015-03-01 10:40:26 -0800

    fixed registration, missed a semi-colon!

--- php/register.php
+++ php/register.php
@@ -20,19 +20,16 @@ echo "Came here <br>";
 		$con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
     	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    	$query = "insert into users values( , :first_name, :user_name, :last_name, :password, :books_sold, :books_bought)";
+    	$query = "insert into users values(null, ?, ?, ?, ?, ?, ?);";
     	$prepared_statement = $con->prepare($query);
     	$prepared_statement->execute(array(
-    		"first_name" => $first_name,
-    		"user_name" => $user_name,
-    		"last_name" => $last_name,
-    		"password" => $password,
-    		"books_sold" => 0,
-    		"books_bought" => 0));
+    		 $first_name,
+    		 $user_name,
+    		 $last_name,
+    		 $password,
+    		 0,
+    		 0));
     	
-    	$prepared_statement->close();
-    	$con->close();
-
-        header('Location: forums.html');
+    	$con = null;
 	}
 ?>
