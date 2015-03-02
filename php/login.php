a7921f9bffa1e93dbbe3b42690368318268fac69
Calvin Ha <calvin.ha@sjsu.edu>
2015-03-01 16:53:51 -0800

    Added try and catch block for PDO
    Set con to null

--- php/login.php
+++ php/login.php
@@ -16,43 +16,51 @@ if(isset($_POST['submit'])) {
         }
     } 
     else{
-    	// Define $email and $password
-    	$username=$_POST['username'];
-    	$password=$_POST['password'];
 
-    	// To protect MySQL injection
-    	$username = stripslashes($username);
-    	$password = stripslashes($password);
-    	$username = mysql_real_escape_string($username);
-    	$password = mysql_real_escape_string($password);
+        try{
+            // Define $email and $password
+            $username=$_POST['username'];
+            $password=$_POST['password'];
 
-    	//db connection
-        $con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
-    	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // To protect MySQL injection
+            $username = stripslashes($username);
+            $password = stripslashes($password);
+            $username = mysql_real_escape_string($username);
+            $password = mysql_real_escape_string($password);
+
+            //db connection
+            $con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             	
-    	//Using PHP prepared statements
-        $query = "SELECT first_name, last_name, password FROM users WHERE user_name = :username";
-        $ps = $con->prepare($query);
+            //Using PHP prepared statements
+            $query = "SELECT first_name, last_name, password FROM users WHERE user_name = :username";
+            $ps = $con->prepare($query);
 
-        //Using PHP prepared statements
-        $ps->execute(array(':username' => $username));
-        $data = $ps-> fetchAll(PDO::FETCH_ASSOC);
+            //Using PHP prepared statements
+            $ps->execute(array(':username' => $username));
+            $data = $ps-> fetchAll(PDO::FETCH_ASSOC);
         
-        //if the array is empty the username is invalid
-        if(empty($data))
-            echo "<h3>You are not a user</h3>";
-        else{
-            //Get the correct password from the database
-            $db_password = $data[0]['password'];
-            
-            if($db_password == $password){
-                $firstname = $data[0]['first_name'];
-                $lastname = $data[0]['last_name'];
-                echo "<h3> Welcome back to Book Sale $firstname $lastname !</h3>";
-            }
+            $con = null;
+        
+            //if the array is empty the username is invalid
+            if(empty($data))
+                echo "<h3>You are not a user</h3>";
             else{
-                echo "<h3> Incorrect password";
+                //Get the correct password from the database
+                $db_password = $data[0]['password'];
+            
+                if($db_password == $password){
+                    $firstname = $data[0]['first_name'];
+                    $lastname = $data[0]['last_name'];
+                    echo "<h3> Welcome back to Book Sale $firstname $lastname !</h3>";
+                }
+                else{
+                    echo "<h3> Incorrect password";
+                }
             }
+        }
+        catch(PDOException $ex){
+            echo "Error: " .$ex->getMessage();
         }
     }
  }
