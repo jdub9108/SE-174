5f94697805cd92138a6d3fd0dd12adf9a2194cb9
Calvin Ha <calvin.ha@sjsu.edu>
2015-03-07 21:57:08 -0800

    Add circle animations for extra credit

--- javascript/animatedJump.js
+++ javascript/animatedJump.js
@@ -5,34 +5,34 @@ var ctx = canvas.getContext('2d');
 
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
-const FPS = 30;
+const FPS = 100;
 
-function Circle(x, y, radius){
+function Circle(x, y, radius, color, endSize){
     this.x = x;
     this.y = y;
-    this.startX = x;
-    this.startY = y;
     this.radius = radius;
-    this.startRadius = radius;
-    this.color="blue";
+    this.color= color;
     this.isFull = false;
+    this.endSize = endSize;
+    this.xDirection = 4;
+    this.yDirection = 4;
     
-    Circle.prototype.draw = function (x, y, radius){
+    Circle.prototype.draw = function (){
        
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 2*Math.PI, false);
+        ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false);
         ctx.fill(); 
     }
 
     Circle.prototype.animate = function (){
 
-        ctx.clearRect(0,0,WIDTH,HEIGHT);
 
-        if ( this.radius*2 <= 0) {
+
+        if ( this.radius*2 <= 10) {
             this.isFull = false;
         }
-        else if(this.radius*2 >= HEIGHT){
+        else if(this.radius*2 >= this.endSize){
             this.isFull = true
         }
         
@@ -41,24 +41,61 @@ function Circle(x, y, radius){
         else if(this.isFull)
             this.radius -=2;
 
-
-        console.log(this.radius*2);
-        this.draw(this.x, this.y, this.radius);
+        this.draw();
         
     }
+
+    Circle.prototype.changeDirection = function(){
+        if(this.x - this.radius <= 0 || this.x + this.radius >= WIDTH)
+            this.xDirection = -this.xDirection;
+        if(this.y - this.radius <= 0 || this.y + this.radius >= HEIGHT)
+            this.yDirection = -this.yDirection;
+    };
+
+    Circle.prototype.move = function(){
+        this.draw();
+        this.changeDirection();
+        this.x += this.xDirection;
+        this.y += this.yDirection;
+    };
 
 }
 
 
-var ball = new Circle(WIDTH/2,HEIGHT/2,100);
+
+var bigBall =  new Circle(WIDTH/2, HEIGHT/2, 10, "blue", HEIGHT);
+var topLeftBall = new Circle(WIDTH/10, HEIGHT/4, 35, "gold", HEIGHT/4);
+var topRightBall = new Circle(.9*WIDTH, HEIGHT/4, 10, "gold", HEIGHT/4);
+
+var bottomLeftBall = new Circle(WIDTH/10, HEIGHT*.85, 10, "gold", HEIGHT/4);
+var bottomRightBall = new Circle(.9*WIDTH, HEIGHT*.85, 10, "gold", HEIGHT/4);
+
+
+var movingBall = new Circle(WIDTH/10, HEIGHT/4, 20, "red", HEIGHT);
+
+var ballsArray = [topLeftBall, bigBall, topRightBall, bottomLeftBall, bottomRightBall];
+
 
 function run(){
     
     setTimeout(function() {
+        //Always clear the canvas before running the animations
+        //Clearing the canvas should NOT be in the other classes, only in the run function
+        ctx.clearRect(0,0,WIDTH,HEIGHT);
+  
+        for(var i = 0; i < ballsArray.length; i++) {
+            //use clousure to assure every ball referenced is the correct ball
+            (function(index){
+                var ball = ballsArray[index];
+                ball.animate();
+                                   
+            })(i);
+
+        }
+        movingBall.move();        
+
         requestAnimationFrame(run);
-        ball.animate();
-    }, 1000 / FPS);
-                            
+    }, 1000 / FPS);                            
 }
 
 
@@ -67,6 +104,7 @@ run();
 
 
 //Here is the src we used: http://css-tricks.com/snippets/jquery/smooth-scrolling/
+//This is for Recently Added on the homepage where the page jump is an animation
 
 $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
