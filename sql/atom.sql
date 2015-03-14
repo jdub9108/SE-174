909950c1c599291bc73edc5c3a9781d35708e9f7
Calvin Ha <calvin.ha@sjsu.edu>
2015-03-13 22:16:17 -0700

    Add new database with better passwords

---
+++ sql/atom.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 5.6.21, for osx10.6 (x86_64)
+--
+-- Host: localhost    Database: atom
+-- ------------------------------------------------------
+-- Server version	5.6.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book_posts`
+--
+
+DROP TABLE IF EXISTS `book_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_posts` (
+  `book_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`book_id`,`post_id`),
+  KEY `posts_fk_id` (`post_id`),
+  CONSTRAINT `books_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
+  CONSTRAINT `posts_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_posts`
+--
+
+LOCK TABLES `book_posts` WRITE;
+/*!40000 ALTER TABLE `book_posts` DISABLE KEYS */;
+INSERT INTO `book_posts` VALUES (1,1),(2,2),(3,3),(4,4);
+/*!40000 ALTER TABLE `book_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `author_first` varchar(100) NOT NULL,
+  `author_last` varchar(100) NOT NULL,
+  `year_published` int(5) NOT NULL,
+  `pages` int(5) NOT NULL,
+  `ISBN` int(13) NOT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'HTML5 and CSS3 for Dummies','Andy','Harris',2015,1000,2147483647),(2,'Practical C Programming','Steve','Oualline',1997,400,2147483647),(3,'Java in Easy Steps','Mike','Mcgrath',2008,100,2147483647),(4,'Programming in C# 3.0','Jesse','Donald',2001,700,2147483647);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `post_id` int(100) NOT NULL AUTO_INCREMENT,
+  `user` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `content` text NOT NULL,
+  `post_closed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'dmevada','2015-03-01','test',0),(2,'brandonj','2015-03-02','test post',0),(3,'calvinh','2015-03-03','test post 2',0),(4,'jordanw','2015-03-04','test post 3',0);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_posts`
+--
+
+DROP TABLE IF EXISTS `user_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_posts` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`post_id`),
+  KEY `post_fk_id` (`post_id`),
+  CONSTRAINT `post_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
+  CONSTRAINT `users_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_posts`
+--
+
+LOCK TABLES `user_posts` WRITE;
+/*!40000 ALTER TABLE `user_posts` DISABLE KEYS */;
+INSERT INTO `user_posts` VALUES (4,1),(5,2),(6,3),(7,4);
+/*!40000 ALTER TABLE `user_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(25) NOT NULL,
+  `user_name` varchar(25) DEFAULT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `books_sold` int(10) NOT NULL,
+  `books_bought` int(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'Dhruv','dmevada','Mevada','dmevada@gmail.com','ehoduc.blr',0,0),(5,'Brandon','brandonj','Jaculina','bjaculina@gmail.com','uxr.xNSxgrccg',0,0),(6,'Calvin','calvinh','Ha','cha@gmail.com','aVery_coolpw',0,0),(7,'Jordan','jordanw','Watts','jwatts@gmail.com','gTrTc&&euanee',0,0),(8,'Zachary','zmartin08','Martin','zmartin@gmail.com','ogpU72Vvjobevc',0,0),(9,'Sally','ssmithy007','Sue','sallysue007@yahoo.com','jamesbondis007',0,0),(10,'John','jsmithy','Smith','johnsmith08@gmail.com','aaaaaaaa',0,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-03-13 22:15:39
