c4a40ac9e975bb73d8f2ec3663d93f8bffe7ab74
Calvin Ha <calvin.ha@sjsu.edu>
2015-03-22 16:39:27 -0700

    Add constants declarations - no more magic numbers!
    Slide 2 elements now animate by moving to the center

--- javascript/animatedJump.js
+++ javascript/animatedJump.js
@@ -1,3 +1,13 @@
+//For all the slide transitions
+const DIV_OFFSCREEN_PERCENTAGE = 150;
+const SLIDE_SPEED = 350
+
+//For slide 2
+const TIME_TO_MIDDLE = 1100;
+const INITIAL_SLIDE_TIME = 300;
+const SLIDE_DELAY_INCREMENT_VALUE = INITIAL_SLIDE_TIME*2;
+
+//jQuery 
 $(document).ready(function() {
     
     var array = ["slide1", "slide2", "slide3"];
@@ -13,33 +23,57 @@ $(document).ready(function() {
         array.push(slide);
         //remove the slide
         slide = array.splice(0,1);
-        showSlide(slide);        
+        showSlide(slide);
+
+        if (slide == "slide2"){
+            
+            var slideTime = INITIAL_SLIDE_TIME;
+            //move the slide then increment the div
+            moveCenter("isbn-slide", slideTime);
+            slideTime += SLIDE_DELAY_INCREMENT_VALUE;
+            
+            moveCenter("author-slide", slideTime);
+            slideTime += SLIDE_DELAY_INCREMENT_VALUE;
+            
+            moveCenter("title-slide", slideTime);
+        }
 
     });
 
+    /*
+      Moves HTML elements to the middle of the screen
+     */
+    function moveCenter(slide, time) {
+        
+        setTimeout(function(){
+            $("#"+slide).animate({
+                "margin-left": "0%" 
+            }, TIME_TO_MIDDLE);
+        }, time);           
+    }
 
+    
     function moveLeft(slide){
+
+        var resetPercentage = "-" + DIV_OFFSCREEN_PERCENTAGE + "%";
         
         $("#"+slide).animate({
-            left: "-150%"
+            "left": resetPercentage //-150%
         }, 400, function() {
+            resetPercentage = resetPercentage.substring(1);
             //important function callback to move the slide back to the right
-            $(this).css("left", "150%"); 
-        });
-
-        
+            $(this).css("left", resetPercentage); //150%
+        });        
     }
     
     function showSlide(slide){
         
         $("#"+slide).animate({
-            left: "0%"
-        }, 350);
+            "left": "0%"
+        }, SLIDE_SPEED);
     }
-
     
 });
-
 
 
 //Here is the src we used: http://css-tricks.com/snippets/jquery/smooth-scrolling/
