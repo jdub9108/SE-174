286282b09ebbb4bafd820c02389fe272d217b9db
Calvin Ha <calvin.ha@sjsu.edu>
2015-04-19 15:25:24 -0700

    Modify database with more books and images
    Please everyone update your database with this one

--- sql/atom.sql
+++ sql/atom.sql
@@ -1,242 +1,196 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.21, for osx10.6 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 15, 2015 at 04:40 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: atom
+-- ------------------------------------------------------
+-- Server version	5.6.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `atom`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `books`
---
-
-CREATE TABLE IF NOT EXISTS `books` (
-`book_id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `author_first` varchar(100) NOT NULL,
-  `author_last` varchar(100) NOT NULL,
-  `year_published` int(5) NOT NULL,
-  `pages` int(5) NOT NULL,
-  `ISBN` int(13) NOT NULL,
-  `image_path` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`book_id`, `title`, `author_first`, `author_last`, `year_published`, `pages`, `ISBN`, `image_path`) VALUES
-(1, 'HTML5 and CSS3 for Dummies', 'Andy', 'Harris', 2015, 1000, 2147483646, ''),
-(2, 'Practical C Programming', 'Steve', 'Oualline', 1997, 400, 2147483641, ''),
-(3, 'Java in Easy Steps', 'Mike', 'Mcgrath', 2008, 100, 2147483636, ''),
-(4, 'Programming in C# 3.0', 'Jesse', 'Donald', 2001, 700, 2147483647, '');
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `book_posts`
 --
 
-CREATE TABLE IF NOT EXISTS `book_posts` (
+DROP TABLE IF EXISTS `book_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_posts` (
   `book_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`book_id`,`post_id`),
+  KEY `posts_fk_id` (`post_id`),
+  CONSTRAINT `books_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
+  CONSTRAINT `posts_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `book_posts`
 --
 
-INSERT INTO `book_posts` (`book_id`, `post_id`) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
+LOCK TABLES `book_posts` WRITE;
+/*!40000 ALTER TABLE `book_posts` DISABLE KEYS */;
+INSERT INTO `book_posts` VALUES (1,1),(2,2),(3,3),(4,4);
+/*!40000 ALTER TABLE `book_posts` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `author_first` varchar(100) NOT NULL,
+  `author_last` varchar(100) NOT NULL,
+  `year_published` int(5) NOT NULL,
+  `pages` int(5) NOT NULL,
+  `isbn` bigint(16) NOT NULL,
+  `image_path` varchar(500) NOT NULL,
+  PRIMARY KEY (`book_id`),
+  UNIQUE KEY `ISBN` (`isbn`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'HTML5 and CSS3 for Dummies','Andy','Harris',2015,1000,9781118289389,'images/bookCovers/html5css3.jpg'),(2,'Practical C Programming','Steve','Oualline',1997,400,9781565923065,''),(3,'Java in Easy Steps','Mike','Mcgrath',2011,100,9781840784435,'images/bookCovers/javaEasySteps.jpg'),(4,'Programming in C# 3.0','Jesse','Liberty',2001,700,9780596527433,''),(5,'JavaScript: The Definitive Guide','David','Flanagan',2001,900,9780596000486,'images/bookCovers/javascriptDefiniteGuide.jpg');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `posts`
 --
 
-CREATE TABLE IF NOT EXISTS `posts` (
-`post_id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `post_id` int(100) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL,
   `date` date NOT NULL,
   `content` text NOT NULL,
-  `post_closed` tinyint(1) NOT NULL
+  `post_closed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`post_id`, `user`, `date`, `content`, `post_closed`) VALUES
-(1, 'dmevada', '2015-03-01', 'test', 0),
-(2, 'brandonj', '2015-03-02', 'test post', 0),
-(3, 'calvinh', '2015-03-03', 'test post 2', 0),
-(4, 'jordanw', '2015-03-04', 'test post 3', 0);
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'dmevada','2015-03-01','test',0),(2,'brandonj','2015-03-02','test post',0),(3,'calvinh','2015-03-03','test post 2',0),(4,'jordanw','2015-03-04','test post 3',0);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `user_books`
+--
+
+DROP TABLE IF EXISTS `user_books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_books` (
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`book_id`),
+  KEY `book_fk_id` (`book_id`),
+  CONSTRAINT `book_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`),
+  CONSTRAINT `user_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_books`
+--
+
+LOCK TABLES `user_books` WRITE;
+/*!40000 ALTER TABLE `user_books` DISABLE KEYS */;
+INSERT INTO `user_books` VALUES (4,1),(4,2),(5,3),(5,4),(8,5);
+/*!40000 ALTER TABLE `user_books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_posts`
+--
+
+DROP TABLE IF EXISTS `user_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_posts` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`post_id`),
+  KEY `post_fk_id` (`post_id`),
+  CONSTRAINT `post_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
+  CONSTRAINT `users_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_posts`
+--
+
+LOCK TABLES `user_posts` WRITE;
+/*!40000 ALTER TABLE `user_posts` DISABLE KEYS */;
+INSERT INTO `user_posts` VALUES (4,1),(5,2),(6,3),(7,4);
+/*!40000 ALTER TABLE `user_posts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`user_id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(25) NOT NULL,
   `user_name` varchar(25) DEFAULT NULL,
   `last_name` varchar(25) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` varchar(25) DEFAULT NULL,
   `books_sold` int(10) NOT NULL,
-  `books_bought` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  `books_bought` int(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `first_name`, `user_name`, `last_name`, `email`, `password`, `books_sold`, `books_bought`) VALUES
-(4, 'Dhruv', 'dmevada', 'Mevada', 'dmevada@gmail.com', '1', 0, 0),
-(5, 'Brandon', 'brandonj', 'Jaculina', 'bjaculina@gmail.com', '2', 0, 0),
-(6, 'Calvin', 'calvinh', 'Ha', 'cha@gmail.com', '3', 0, 0),
-(7, 'Jordan', 'jordanw', 'Watts', 'jwatts@gmail.com', '4', 0, 0),
-(8, 'Zachary', 'zmartin08', 'Martin', 'zmartin@gmail.com', 'ogpU72Vvjobevc', 0, 0),
-(9, '', '', '', NULL, '', 0, 0);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'Dhruv','dmevada','Mevada','dmevada@gmail.com','aaaaaaaa',0,0),(5,'Brandon','brandonj','Jaculina','bjaculina@gmail.com','aaaaaaaa',0,0),(6,'Calvin','calvinh','Ha','cha@gmail.com','aaaaaaaa',0,0),(7,'Jordan','jordanw','Watts','jwatts@gmail.com','aaaaaaaa',0,0),(8,'Zachary','zmartin08','Martin','zmartin@gmail.com','ogpU72Vvjobevc',0,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_books`
---
-
-CREATE TABLE IF NOT EXISTS `user_books` (
-  `user_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_posts`
---
-
-CREATE TABLE IF NOT EXISTS `user_posts` (
-  `user_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_posts`
---
-
-INSERT INTO `user_posts` (`user_id`, `post_id`) VALUES
-(4, 1),
-(5, 2),
-(6, 3),
-(7, 4);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
- ADD PRIMARY KEY (`book_id`), ADD UNIQUE KEY `ISBN` (`ISBN`);
-
---
--- Indexes for table `book_posts`
---
-ALTER TABLE `book_posts`
- ADD PRIMARY KEY (`book_id`,`post_id`), ADD KEY `posts_fk_id` (`post_id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
- ADD PRIMARY KEY (`post_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `user_books`
---
-ALTER TABLE `user_books`
- ADD PRIMARY KEY (`user_id`,`book_id`), ADD KEY `book_fk_id` (`book_id`);
-
---
--- Indexes for table `user_posts`
---
-ALTER TABLE `user_posts`
- ADD PRIMARY KEY (`user_id`,`post_id`), ADD KEY `post_fk_id` (`post_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-MODIFY `post_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `book_posts`
---
-ALTER TABLE `book_posts`
-ADD CONSTRAINT `books_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
-ADD CONSTRAINT `posts_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `user_books`
---
-ALTER TABLE `user_books`
-ADD CONSTRAINT `book_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`),
-ADD CONSTRAINT `user_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `user_posts`
---
-ALTER TABLE `user_posts`
-ADD CONSTRAINT `post_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
-ADD CONSTRAINT `users_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-19 15:23:33
