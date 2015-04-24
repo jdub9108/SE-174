4d11719c0613bde5d388eb8e113a894f6bae4448
Dhruv Mevada <mevadadhruv90@gmail.com>
2015-04-24 07:00:55 -0700

    updated register.php to prevent duplicate usernames while registering

--- php/register.php
+++ php/register.php
@@ -15,17 +15,28 @@ include 'header.php';
 		$con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
     	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // PHP Prepared Statements
-    	$query = "insert into users values(null, :first_name, :user_name, :last_name, :email,  :password, :books_sold, :books_bought);";
-        $prepared_statement = $con->prepare($query);        
-        $prepared_statement->bindValue(':first_name', $first_name, PDO::PARAM_STR);
-        $prepared_statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
-        $prepared_statement->bindValue(':last_name', $last_name, PDO::PARAM_STR);
-        $prepared_statement->bindValue(':email', $email, PDO::PARAM_STR);
-        $prepared_statement->bindValue(':password', $password, PDO::PARAM_STR);
-        $prepared_statement->bindValue(':books_sold', 0, PDO::PARAM_INT);
-        $prepared_statement->bindValue(':books_bought', 0, PDO::PARAM_INT);
-        $prepared_statement->execute();
+        try 
+        {
+            $query = "insert into users values(null, :first_name, :user_name, :last_name, :email,  :password, :books_sold, :books_bought);";
+            $prepared_statement = $con->prepare($query);        
+            $prepared_statement->bindValue(':first_name', $first_name, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':last_name', $last_name, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':email', $email, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':password', $password, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':books_sold', 0, PDO::PARAM_INT);
+            $prepared_statement->bindValue(':books_bought', 0, PDO::PARAM_INT);
+            $prepared_statement->execute();
+        } 
+
+        catch (PDOException $e) 
+        {
+            if($e->errorInfo[1] == 1062)
+            {
+                echo "<script type='text/javascript'> alert('Sorry, that username is already taken'); </script>";
+                exit();
+            }
+        }    
         
     	$con = null;
 
