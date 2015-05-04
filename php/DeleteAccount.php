5841fc45a1179f3d7c59bed736c75b1a8027d339
Jordan Watts <jdub9108@yahoo.com>
2015-05-03 21:55:38 -0700

    started back end for delete account

---
+++ php/DeleteAccount.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include 'header.php';
+
+session_start();
+
+if(!isset($_SESSION['username'])) 
+{
+  header('Location: forums.php');
+}
+
+try 
+{
+    $con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "DELETE FROM users WHERE user_name = :username";
+    $ps = $con->prepare($query);
+
+    $ps->execute(array(':username' => $_SESSION['username']));
+    
+    include 'logout.php';
+}
+catch(PDOException $ex)
+{
+    echo "Error: " .$ex->getMessage();
+}
+?>
