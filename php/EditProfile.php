04814827fc02b287bcbfcb2cb7bcfc480ed0ec14
Jordan Watts <jdub9108@yahoo.com>
2015-05-04 20:04:23 -0700

    started edit profile to put delete account in

---
+++ php/EditProfile.php
@@ -0,0 +1,162 @@
+<?php session_start();
+    if(!isset($_SESSION['username']))
+    {
+        header('Location: index.php');
+    }
+    include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <title>Edit Profile</title>
+
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <script src="../javascript/utils.js"></script>
+  </head>   
+
+  <body>
+    <div class="main_div">
+      
+      <div class="top-bar">
+        <div>
+          <a href="index.php"> <img id="logo" src= "../images/book-logo3.png"> </a>
+        </div>
+        <ul>
+          <li> <a href="index.php"> Home </a></li>
+          <li> <a href="about.php">About</a></li>
+          <li> <?php echo $_SESSION['username']; ?> 
+                <ul>
+                        <li><a href='EditProfile.php'> Edit Profile </a></li>
+                        <li> Settings </li>
+                        <li><a href='logout.php'> Logout </a></li>
+                </ul>
+          </li>
+        </ul>
+      </div>
+     <!-- <img id="banner" src= "images/booksalesjsu.png"> -->
+      
+      <!-- menu bar
+      <nav class="menu_bar">
+        <a href="index.html">Home</a>
+        <a href="">Browse</a>
+        <a href="index.html#RecentlyAdded">Recently Added</a>
+        <a href="login.html">Login</a>
+      </nav> -->
+      
+      <div class="v-menu-bar-div">
+        <ul class="vertical-menu-bar">
+          <li class="v-menu-a"> <a href="">About</a> </li> 
+          <li class="v-menu-a"> <a href="">Contact Us</a> </li> 
+          <li class="v-menu-a"> <a href="">Lorum Ipsum</a> </li>
+          <li class="v-menu-a"> <a href="http://www.sjsu.edu">SJSU</a> </li>
+        </ul>
+      </div>
+      
+      <div class="profile-display">
+        <ul>
+          <?php displayProfile(); ?>
+        </ul>
+      </div>
+
+      <div class="user-info" id="registration-height">
+        <h2>Update Information</h2>       
+        <form action="" id="editForm" name="editForm" method="post" onsubmit="return validateRegistration()">
+          <input type="text" class="inputField editPage" name="firstName" placeholder="  First Name: John" >
+          <input type="text" class="inputField editPage" name="lastName" placeholder="  Last Name: Smith" >
+          <input type="text" class="inputField editPage" name="email" placeholder="  Email: john.smith@example.com" >
+          <input type="text" class="inputField editPage" name="userName" placeHolder= " Username:  JSmith123" > 
+          <input type="password" class="inputField editPage" name="password" placeholder= "  Password" >
+          <input type="password" class="inputField editPage" name="repeatPassword" placeholder= "  Repeat Password" >
+          <button class="request-button" type="submit" form="registrationForm" name="submit" value="submit"> Update! </button>
+        </form>
+      </div>        
+    </div>  
+
+    <?php
+
+    if(isset($_POST['submit']))
+    {
+        editProfile();
+    }
+
+    function displayProfile()
+    {
+        $con1 = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
+        $con1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        try {
+            $query = "SELECT first_name, last_name, user_name, email FROM users WHERE user_name = :user_name";
+            $prepared_statement = $con1->prepare($query);
+            $prepared_statement->bindValue(':user_name', $_SESSION['username'], PDO::PARAM_STR);
+            $prepared_statement->execute();
+            
+            $result = $prepared_statement->fetch(PDO::FETCH_ASSOC);
+            
+            echo '<li> First Name: '. $result['first_name'] . '</li>';
+            echo '<li> Last Name: '. $result['last_name'] . '</li>';
+            echo '<li> Userame: '. $result['user_name'] . '</li>';
+            echo '<li> Email: '. $result['email'] . '</li>';
+            
+            
+        } 
+        catch (Exception $e) 
+        {
+            echo "<script type='text/javascript'> alert('Sorry, there is no account information'); </script>";
+            exit();
+        }
+        
+        $con1 = null;
+    }
+    
+    function editProfile()
+    {
+        $email = $_POST['email'];
+        $crypt = better_crypt($_POST['password']);
+        $first_name = $_POST['firstName'];
+        $last_name = $_POST['lastName'];
+        $email=$_POST['email'];
+        $user_name = $_POST['userName'];
+
+        $con = new PDO("mysql:host=localhost;dbname=".DATABASE_NAME, DATABASE_NAME, PASSWORD);
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        try
+        {   
+            $query = "insert into users values(null, :first_name, :user_name, :last_name, :email, :password, :books_sold, :books_bought);";
+            $prepared_statement = $con->prepare($query);        
+            $prepared_statement->bindValue(':first_name', $first_name, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':last_name', $last_name, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':email', $email, PDO::PARAM_STR);
+            $prepared_statement->bindValue(':password', $crypt, PDO::PARAM_STR);
+            $prepared_statement->execute();
+
+            $_SESSION['username'] = $_POST['userName'];
+            header("Location: forums.php");
+        } 
+
+        catch (Exception $e) 
+        {
+            echo "<script type='text/javascript'> alert('Sorry, that username is already taken'); </script>";
+            exit();
+        }
+        
+        $con = null;
+    }
+    
+    //encryption algorithm
+    function better_crypt($input, $rounds = 7)
+    {
+        $salt = "";
+        $salt_chars = array_merge(range('A','Z'), range('a','z'), range(0,9));
+        for($i=0; $i < 22; $i++) {
+          $salt .= $salt_chars[array_rand($salt_chars)];
+        }
+        return crypt($input, sprintf('$2a$%02d$', $rounds) . $salt);
+    }
+?>
+
+  </body>   
+</html>
