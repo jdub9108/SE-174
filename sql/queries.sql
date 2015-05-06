5bd7717d0f5d2aba226085bea923dc0a18e64e7f
Dhruv Mevada <mevadadhruv90@gmail.com>
2015-05-05 22:37:49 -0700

    added the book and user connection queries for user submittedbooks

--- sql/queries.sql
+++ sql/queries.sql
@@ -13,3 +13,11 @@ from user_posts, users, book_posts, posts
 where user_posts.user_id = users.user_id 
 and user_posts.post_id = posts.post_id
 group by first_name;
+
+--inserts a user submitted to the database, and links the connection between the user
+--and the submitted book. These queries have to occur in the order given below.
+
+insert into books values(null, :title, :author_first, :author_last, :year_published, :pages, :isbn, :image_path);
+select user_id from users where user_name = :user_name	
+select book_id from books where title = :title
+insert into user_books values(:user_id, :book_id)
