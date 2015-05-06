1dd8d6ac2d706f2adfb59ffa28de79fc3d542614
Calvin Ha <calvin.ha@sjsu.edu>
2015-05-06 00:26:43 -0700

    Add left menu bar
    Link index.css file

--- php/forums.php
+++ php/forums.php
@@ -18,23 +18,35 @@ if(!isset($_SESSION['username']))
 		<meta charset="UTF-8">
     	<title>Forums</title>
     	<link rel='stylesheet' type='text/css' href='../css/forums.css'>
+        <link rel='stylesheet' type='text/css' href='../css/index.css'>
 	</head>
 
 
 	<body>
-		<header>
-			<div class='top-bar'>
+          <div class="main_div">
+	  <header>
+	    <div class='top-bar'>
 	      <ul>
 	        <li> <?php echo $_SESSION['username']; ?> 
-	        	<ul>
-							<li><a href='EditProfile.php'> Settings </a></li>
-	        		<li><a href='logout.php'> Logout </a></li>
-	        	</ul>
+	          <ul>
+		    <li><a href='EditProfile.php'> Settings </a></li>
+	            <li><a href='logout.php'> Logout </a></li>
+	          </ul>
 	        </li>
 	      </ul>
-		  </div>
+	    </div>
 	  </header>
 
+             <div id="left-menu-bar"> 
+               <ul>
+                 <li>Home</li>
+                 <li><a href="">Your Books</a></li>
+                 <li><a href="">Upload a Book</a></li>
+                 <li><a href="">View all Books</a></li>
+                 <li><a href="">Manange Account</a></li>
+               </ul> 
+             </div>
+                
       <div class='book-info' id='add-book-height'>
         <h2>Add Book </h2>        
         <form action='AddBook.php' method='post' id='addBookForm'> <!-- add js and php here -->
@@ -47,7 +59,6 @@ if(!isset($_SESSION['username']))
           <button class='request-button' name='submit' type='submit' form= 'addBookForm' value= 'submit'> Add book! </button>
         </form>
       </div> 
-
-
+      </div>
 	</body>
 </html>
