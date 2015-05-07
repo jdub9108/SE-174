29bafeddff053a91e46fd104fc3ba8efb3ed22f4
Brandon Jaculina <xbeejay24x@yahoo.com>
2015-05-06 21:06:10 -0700

    The new database

--- sql/atom.sql
+++ sql/atom.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: May 06, 2015 at 06:41 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Host: localhost
+-- Generation Time: May 07, 2015 at 06:05 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,17 +27,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `books` (
-  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+`book_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `author_first` varchar(100) NOT NULL,
   `author_last` varchar(100) NOT NULL,
   `year_published` int(5) NOT NULL,
   `pages` int(5) NOT NULL,
   `isbn` bigint(16) NOT NULL,
-  `image_path` varchar(500) NOT NULL,
-  PRIMARY KEY (`book_id`),
-  UNIQUE KEY `ISBN` (`isbn`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+  `image_path` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `books`
@@ -51,13 +49,8 @@ INSERT INTO `books` (`book_id`, `title`, `author_first`, `author_last`, `year_pu
 (5, 'JavaScript: The Definitive Guide', 'David', 'Flanagan', 2001, 900, 9780596000486, 'images/bookCovers/javascriptDefiniteGuide.jpg'),
 (6, 'The Ruby Programming Language', 'David', 'Flanagan', 2008, 448, 9780596516178, ''),
 (7, 'jQuery Pocket Reference', 'David', 'Flanagan', 2011, 160, 9781449397227, ''),
-(12, 'html', 'test', 'test', 2014, 415, 9781118289380, ''),
 (13, 'Great Gatsby', 'Jim', 'Gilligan', 2015, 12, 23412341234243, ''),
-(14, 'Great Gatsby', 'Big', 'Watts', 2015, 12, 934800938408, ''),
-(15, 'Playboy', 'Hue', 'Heffner', 2012, 60, 3209348309, ''),
-(17, 'Playboy', 'Hue', 'Heffner', 2012, 60, 790078078097, ''),
-(18, 'Playboy', 'Hue', 'Heffner', 2012, 60, 89978987, ''),
-(19, 'jlsdkjf', 'herp ', 'derp', 2012, 60, 6435764567, '');
+(14, 'Great Gatsby', 'Big', 'Watts', 2015, 12, 934800938408, '');
 
 -- --------------------------------------------------------
 
@@ -67,9 +60,7 @@ INSERT INTO `books` (`book_id`, `title`, `author_first`, `author_last`, `year_pu
 
 CREATE TABLE IF NOT EXISTS `book_posts` (
   `book_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`book_id`,`post_id`),
-  KEY `posts_fk_id` (`post_id`)
+  `post_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -89,13 +80,12 @@ INSERT INTO `book_posts` (`book_id`, `post_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
-  `post_id` int(100) NOT NULL AUTO_INCREMENT,
+`post_id` int(100) NOT NULL,
   `user` varchar(50) NOT NULL,
   `date` date NOT NULL,
   `content` text NOT NULL,
-  `post_closed` tinyint(1) NOT NULL,
-  PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `post_closed` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `posts`
@@ -114,23 +104,25 @@ INSERT INTO `posts` (`post_id`, `user`, `date`, `content`, `post_closed`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+`user_id` int(10) NOT NULL,
   `first_name` varchar(25) CHARACTER SET utf8 NOT NULL,
   `user_name` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
   `last_name` varchar(25) CHARACTER SET utf8 NOT NULL,
   `email` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   `password` varchar(60) CHARACTER SET utf8 DEFAULT NULL,
   `books_sold` int(10) NOT NULL,
-  `books_bought` int(10) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `books_bought` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `first_name`, `user_name`, `last_name`, `email`, `password`, `books_sold`, `books_bought`) VALUES
-(2, 'Jordan', 'jdub9108', 'Watts', 'Jdub9108@gmail.com', '$2a$07$tBEEWQyE8rvtgg6vIPXuMeWqfXxN/.P1Blv6EfwOfvMHKrEpMoZIu', 0, 0);
+(2, 'Jordan', 'jdub9108', 'Watts', 'Jdub9108@gmail.com', '$2a$07$tBEEWQyE8rvtgg6vIPXuMeWqfXxN/.P1Blv6EfwOfvMHKrEpMoZIu', 0, 0),
+(3, 'brandon', 'brandonjay', 'jaculina', 'brandonjay@gmail.com', '$2a$07$BkbPV7xmAlhjs6LKvtFSUODqAVzfo7/8GGJHw1EtaJGJwpDlwW5yy', 0, 0),
+(5, 'Calvin', 'calvinha', 'Ha', 'calvinha@gmail.com', '$2a$07$wPiWKX6iFYNHnOiD7QAOzOT56xHpkUHCEIerIltrz3AdSEQhmidme', 0, 0),
+(6, 'Dhruv', 'dmevada', 'Mevada', 'dhruvmevada@gmail.com', '$2a$07$NSFGKghHtGA0LkSyZQnwTey7FsDtvdrPq3fqtqLm7guSexkJr4rUO', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -140,9 +132,7 @@ INSERT INTO `users` (`user_id`, `first_name`, `user_name`, `last_name`, `email`,
 
 CREATE TABLE IF NOT EXISTS `user_books` (
   `user_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`book_id`),
-  KEY `book_fk_id` (`book_id`)
+  `book_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -150,12 +140,15 @@ CREATE TABLE IF NOT EXISTS `user_books` (
 --
 
 INSERT INTO `user_books` (`user_id`, `book_id`) VALUES
+(3, 1),
+(6, 2),
+(5, 3),
+(2, 4),
+(3, 5),
+(5, 6),
+(6, 7),
 (2, 13),
-(2, 14),
-(2, 15),
-(2, 17),
-(2, 18),
-(2, 19);
+(2, 14);
 
 -- --------------------------------------------------------
 
@@ -165,11 +158,68 @@ INSERT INTO `user_books` (`user_id`, `book_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `user_posts` (
   `user_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`post_id`),
-  KEY `post_fk_id` (`post_id`)
+  `post_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+ ADD PRIMARY KEY (`book_id`), ADD UNIQUE KEY `ISBN` (`isbn`);
+
+--
+-- Indexes for table `book_posts`
+--
+ALTER TABLE `book_posts`
+ ADD PRIMARY KEY (`book_id`,`post_id`), ADD KEY `posts_fk_id` (`post_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+ ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_books`
+--
+ALTER TABLE `user_books`
+ ADD PRIMARY KEY (`user_id`,`book_id`), ADD KEY `book_fk_id` (`book_id`);
+
+--
+-- Indexes for table `user_posts`
+--
+ALTER TABLE `user_posts`
+ ADD PRIMARY KEY (`user_id`,`post_id`), ADD KEY `post_fk_id` (`post_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+MODIFY `post_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
@@ -178,22 +228,22 @@ CREATE TABLE IF NOT EXISTS `user_posts` (
 -- Constraints for table `book_posts`
 --
 ALTER TABLE `book_posts`
-  ADD CONSTRAINT `books_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `posts_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON UPDATE CASCADE;
+ADD CONSTRAINT `books_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
+ADD CONSTRAINT `posts_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_books`
 --
 ALTER TABLE `user_books`
-  ADD CONSTRAINT `book_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `book_fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `user_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_posts`
 --
 ALTER TABLE `user_posts`
-  ADD CONSTRAINT `post_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `post_fk_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `users_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
