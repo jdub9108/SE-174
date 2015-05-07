689ffedf8d44c32b2913feccff1d8392c13ce1aa
Calvin Ha <calvin.ha@sjsu.edu>
2015-05-06 18:33:50 -0700

    Fix show all books that don't belong to user
    Thank you Brandon for the query
    Good job Brandon!

--- php/viewBooks.php
+++ php/viewBooks.php
@@ -7,8 +7,8 @@ include 'searchUtils.php';
 define("WHERE_USER", " WHERE user_name = :user_name AND books.book_id = user_books.book_id AND users.user_id = user_books.user_id ");
 
 //WHERE clause for all books except the users
-define("WHERE_ALL", " WHERE user_name != :user_name AND books.book_id != user_books.book_id AND users.user_id != user_books.user_id ");
-
+define("WHERE_ALL", "WHERE (users.user_name != :user_name) AND (users.user_id = user_books.user_id) and (user_books.book_id = books.book_id) ");
+                     
 
 define ("USER_BOOKS_QUERY", SELECT_QUERY . FROM_QUERY . WHERE_USER );
 define("ALL_BOOKS_QUERY", SELECT_QUERY . FROM_QUERY . WHERE_ALL);
@@ -33,4 +33,4 @@ function showBooks($con, $allBooks) {
     grabBooks($ps);
 }
 
-?>
+?>      
