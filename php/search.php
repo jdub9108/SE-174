72f5a4bcb7043b87b63a442e918e1cc35cbfaa7c
Calvin Ha <calvin.ha@sjsu.edu>
2015-05-10 22:59:28 -0700

    Modify SQL Query to use 'LIKE'
    Users can now search for a portion of a query
    
    For example:
    
    searching 'harris' will return a result
    searching '9' will return all books with ISBN that start with 9

--- php/search.php
+++ php/search.php
@@ -3,18 +3,19 @@
 
 include 'searchUtils.php';
 
-//example query to test
 
-// SELECT title, author_first, author_last, isbn, user_name, year_published, image_path FROM books, user_books, users WHERE (isbn = '9781118289389' OR books.title = '9781118289389' OR (CONCAT (books.author_first, ' ',  books.author_last) = '9781118289389') ) AND books.book_id = user_books.book_id AND users.user_id = user_books.user_id;
+//NEW SEARCH QUERY USING LIKE
+
+//SELECT title, author_first, author_last, isbn, user_name, year_published, image_path FROM books, user_books, users WHERE (isbn LIKE 'flanagan%' OR books.title LIKE 'flanagan%' OR (CONCAT (books.author_first, ' ',  books.author_last) LIKE 'flanagan%')  OR (books.author_first LIKE 'flanagan%') OR (books.author_last LIKE 'flanagan%') ) AND books.book_id = user_books.book_id AND users.user_id = user_books.user_id;
 
 
-define ("WHERE_QUERY",  "WHERE (isbn = :searchTerm OR books.title = :searchTerm OR (CONCAT (books.author_first, ' ',  books.author_last) = :searchTerm ) ) AND books.book_id = user_books.book_id AND users.user_id = user_books.user_id ");
+define ("WHERE_QUERY",  "WHERE (isbn LIKE :searchTerm OR books.title LIKE :searchTerm OR (CONCAT (books.author_first, ' ',  books.author_last) LIKE :searchTerm ) OR (books.author_first LIKE :searchTerm) OR (books.author_last LIKE :searchTerm)) AND books.book_id = user_books.book_id AND users.user_id = user_books.user_id ");
 
 
 function getDataFromJavascript() {
     
     // get the search term from javascript
-    $searchTerm = $_GET['q'];
+    $searchTerm = $_GET['q'] . '%';
     if(preg_match("/^\s+$/", $searchTerm)) { //check if the user entered in blank spaces
         echo NO_RESULTS;
         return;
